e193dca9869ba7c0a1340a4c4cc1e8b859a37aa1
Reinier <reinier7175@gmail.com>
2025-02-15 18:38:12 +0100

    k

--- cs4575_project1/implementations/tensorflow_imp.py
+++ cs4575_project1/implementations/tensorflow_imp.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
+from tensorflow.keras import layers
 
 # 1️⃣ Load & Preprocess MNIST Dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
